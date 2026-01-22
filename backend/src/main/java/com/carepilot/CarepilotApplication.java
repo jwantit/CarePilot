578a4950901d01776b77c71f95e3f988699d7813
KimJiwon <kgw072089@gmail.com>
2026-01-22 17:40:13 +0900

    Init Project

---
+++ backend/src/main/java/com/carepilot/CarepilotApplication.java
@@ -0,0 +1,13 @@
+package com.carepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarepilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarepilotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/carepilot/CarepilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarepilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
