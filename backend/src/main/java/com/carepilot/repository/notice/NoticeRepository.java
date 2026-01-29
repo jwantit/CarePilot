4e5106531570419187a47a0fd15e70431456acce
leegunho <dlrjsgh040603@gmail.com>
2026-01-29 20:14:45 +0900

    [Back] fix : 충돌 해결

--- backend/src/main/java/com/carepilot/repository/notice/NoticeRepository.java
+++ backend/src/main/java/com/carepilot/repository/notice/NoticeRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    long countByIsPinnedTrue();
 
     Page<Notice> findAllByOrderByIsPinnedDescCreatedAtDesc(Pageable pageable);
 }
